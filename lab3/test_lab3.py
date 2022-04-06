2d195b53458f6e4838b568fa4832d943ab0a540d
Matej Cubek <matej.cubek@neos.hr>
2022-04-06 18:37:16 +0200

    test added

---
+++ lab3/test_lab3.py
@@ -0,0 +1,32 @@
+import CF
+
+import unittest
+from unittest import TestCase
+from unittest.mock import patch
+from io import StringIO
+
+
+class TestLab3(TestCase):
+
+    @patch('sys.stdout', new_callable=StringIO)
+    def test_lab(self, stdout):
+        f_input = open('./test/R.in', 'r')
+        f_expected = open('./test/R.out', 'r')
+
+        input_string = f_input.read()
+        expected = f_expected.read()
+
+        f_input.close()
+        f_expected.close()
+
+        with patch('sys.stdin', StringIO(input_string)):
+            CF.main()
+
+            expected = str(expected).replace("\n", ":")
+            result = str(stdout.getvalue()).replace("\n", ":")
+
+            self.assertEqual(expected, result)
+
+
+if __name__ == '__main__':
+    unittest.main()
